b332db61a9f9d249f555e9272fffd58576d6ab37
lucasgragera <lucasgragera51@gmail.com>
2025-08-04 19:58:57 -0300

    First Commit

---
+++ Desktop/tuputn/segundosemestre/programacionll/Programacionll/src/programacionll/TrabajosPracticos/TrabajoPractico1.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programacionll.TrabajosPracticos;
+
+import java.util.Scanner;
+/**
+ * 
+ *
+ * @author lucasgragera
+ */
+public class TrabajoPractico1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //2)
+        //a)
+        //System.out.println("Hola Mundo");
+        
+        //b)
+        //System.out.println("Hola Java!");
+        
+        //3)
+        //a)
+        //String nombre= "Lucas";
+        
+        //b)
+        //int edad = 23;
+        
+        //c)
+        //double altura = 1.75;
+        
+        //d)
+        //boolean estudiante = true;
+        
+        //System.out.println(nombre);
+        //System.out.println(edad);
+        //System.out.println(altura);
+        //System.out.println(estudiante);
+        
+        //4)
+        //Scanner input = new Scanner(System.in); 
+        //String nombre;
+        //int edad;
+        //System.out.println("Ingresa tu nombre:");
+        //nombre = input.nextLine();
+        //System.out.println("Ingresa tu edad:");
+        //edad = Integer.parseInt(input.nextLine());
+        //System.out.println(nombre + " tiene "+ edad + " años");
+        
+        //5)
+        //Scanner input = new Scanner(System.in);
+        //a)
+        //double suma;
+        
+        //b)
+        //double resta;
+        
+        //c)
+        //double multiplicacion;
+        
+        //d)
+        //double division;
+                
+        //int primerNumero;
+        //int segundoNumero;
+        //System.out.println("Ingresa un numero:");
+        //primerNumero = Integer.parseInt(input.nextLine());
+        //System.out.println("Ingresa ingrese otro numero:");
+        //segundoNumero = Integer.parseInt(input.nextLine());
+        
+        //suma = (primerNumero + segundoNumero);
+        //resta = (primerNumero - segundoNumero);
+        //multiplicacion = (primerNumero * segundoNumero);
+        //division = ((primerNumero * 1.0) / segundoNumero);
+        
+        //System.out.println("La suma de " + primerNumero + " y " + segundoNumero + "es: " + suma);
+        //System.out.println("La resta de " + primerNumero + " y " + segundoNumero + "es: " + resta);
+        //System.out.println("La multiplicacion de " + primerNumero + " y " + segundoNumero + "es: " + multiplicacion);
+        //System.out.println("La division de " + primerNumero + " y " + segundoNumero + "es: " + division);
+
+        //6)
+        //System.out.println("Nombre: Nombre: Juan Pérez\n" + "Edad: 30 años\n" + "Dirección: \"Calle Falsa 123\"");
+    
+        //8) a) 
+        //Scanner input = new Scanner(System.in);
+        //int division;
+                
+        //int primerNumero;
+        //int segundoNumero;
+        //System.out.println("Ingresa un numero:");
+        //primerNumero = Integer.parseInt(input.nextLine());
+        //System.out.println("Ingresa ingrese otro numero:");
+        //segundoNumero = Integer.parseInt(input.nextLine());
+        
+        //division = ((primerNumero) / segundoNumero);
+        
+        //System.out.println("La division de " + primerNumero + " y " + segundoNumero + " es: " + division);
+        
+        //8) b)
+        //Scanner input = new Scanner(System.in);
+        //double division;
+                
+        //int primerNumero;
+        //int segundoNumero;
+        //System.out.println("Ingresa un numero:");
+        //primerNumero = Integer.parseInt(input.nextLine());
+        //System.out.println("Ingresa ingrese otro numero:");
+        //segundoNumero = Integer.parseInt(input.nextLine());
+        
+        //division = ((primerNumero * 1.0) / segundoNumero);
+        
+        //System.out.println("La division de " + primerNumero + " y " + segundoNumero + " es: " + division);
+    
+        //9)
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.print("Ingresa tu nombre: ");
+        //String nombre = scanner.nextLine(); // Seria nextLine no nextInt ya que es un String.
+        //System.out.println("Hola, " + nombre);
+
+        //10)
+        //int a = 5;
+        //int b = 2;
+        //int resultado = a / b;
+        //System.out.println("Resultado: " + resultado);
+    }
+    
+}
